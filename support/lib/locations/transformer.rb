ce019b767b7636b3fd2c99fd536c1dd8da61a43a
Rex Feng <rexfeng@gmail.com>
2014-05-08 00:49:42 -0400

    add bin/reverse_build to allow json to yml transform

--- support/lib/locations/transformer.rb
+++ support/lib/locations/transformer.rb
@@ -10,19 +10,39 @@ module Locations
     end
 
     def loaded_data
-      YAML::load_file(@input)
+      if file_is_json?(@input)
+        JSON.parse(File.read(@input))
+      elsif file_is_yml?(@input)
+        YAML::load_file(@input)
+      end
     end
 
     def write!
       File.open(@output, "w") do |f|
-        f.write(pretty_json)
+        if file_is_json?(@output)
+          f.write(pretty_json)
+        elsif file_is_yml?(@output)
+          f.write(pretty_yaml)
+        end
       end
     end
 
     private
 
+    def pretty_yaml
+      loaded_data.to_yaml
+    end
+
     def pretty_json
       JSON.pretty_generate(loaded_data)
+    end
+
+    def file_is_yml?(file)
+      file.split('.').last.include?('yml')
+    end
+
+    def file_is_json?(file)
+      file.split('.').last.include?('json')
     end
 
   end
